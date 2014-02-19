1576a069aa4e08d1388dbf0859796b8dfb3040cc
George Bafaloukas <george.bafaloukas@caxtonfx.com>
2014-02-19 12:39:31 +0000

    Contact Us VC

---
+++ Caxtonfx/Class/ContactVC/ContactVC.h
@@ -0,0 +1,13 @@
+//
+//  ContactVC.h
+//  Caxtonfx
+//
+//  Created by George Bafaloukas on 17/02/2014.
+//  Copyright (c) 2014 kipl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactVC : UIViewController
+
+@end
