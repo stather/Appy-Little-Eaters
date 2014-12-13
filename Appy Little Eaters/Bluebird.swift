6c97c009e58488054d81d415d8cae45cf662f295
Russell Stather <russell.stather@mac.com>
2014-12-13 12:22:08 +0000

    Move characters generalised

--- Appy Little Eaters/Bluebird.swift
+++ Appy Little Eaters/Bluebird.swift
@@ -18,6 +18,16 @@ public class BlueBird : ForestCreature, Performer, MoveableProtocol{
 		case FlyingLeft
 	}
 	
+	public enum HorizontalDirection{
+		case Left
+		case Right
+	}
+	
+	public enum VerticalDirection{
+		case Up
+		case Down
+	}
+	
 	var location:BirdLocation = BirdLocation.SittingOnRight
 	
 	convenience init(){
@@ -43,11 +53,15 @@ public class BlueBird : ForestCreature, Performer, MoveableProtocol{
 	}
 	
 	var destination:CGPoint!
+	var horizontalDirection:HorizontalDirection!
+	var verticalDirection:VerticalDirection!
 	
 	func flyToLeftTree(){
 		xScale = 1
 		removeAllActions()
 		destination = CGPointMake(2727, 1035 - 389)
+		horizontalDirection = HorizontalDirection.Left
+		verticalDirection = VerticalDirection.Down
 		location = BirdLocation.FlyingLeft
 		runAction(flappingBird(), withKey: "flapping")
 		
@@ -57,6 +71,8 @@ public class BlueBird : ForestCreature, Performer, MoveableProtocol{
 		xScale = -1
 		removeAllActions()
 		destination = CGPointMake(4448, 1035 - 217)
+		horizontalDirection = HorizontalDirection.Right
+		verticalDirection = VerticalDirection.Up
 		location = BirdLocation.FlyingRight
 		runAction(flappingBird(), withKey: "flapping")
 		
@@ -76,86 +92,88 @@ public class BlueBird : ForestCreature, Performer, MoveableProtocol{
 			break
 		}
 	}
-	
-	func flyLeft(amount: Float){
+
+	func fly(amount: Float){
 		var x:Float = Float(position.x)
 		var y:Float = Float(position.y)
 		var original = forestScene.originalPoint(position)
 		var howMuch = forestScene.fact * ForestScene.backgroundWidth() / 10
-		if original.x > destination.x{
-			x -= amount * 25
-			if x < -howMuch*2{
-				x += howMuch * 10
+		if horizontalDirection == HorizontalDirection.Left{
+			if original.x > destination.x{
+				x -= amount * 25
+				if x < -howMuch*2{
+					x += howMuch * 10
+				}
+				if x > howMuch*8{
+					x -= howMuch * 10
+				}
 			}
-			if x > howMuch*8{
-				x -= howMuch * 10
+		}else{
+			if original.x < destination.x{
+				x += amount * 25
+				if x < -howMuch*2{
+					x += howMuch * 10
+				}
+				if x > howMuch*8{
+					x -= howMuch * 10
+				}
 			}
 		}
-		if original.y > destination.y{
-			y -= amount * 25
+		if verticalDirection == VerticalDirection.Down{
+			if original.y > destination.y{
+				y -= amount * 25
+			}
+		}else{
+			if original.y < destination.y{
+				y += amount * 25
+			}
 		}
 		
 		position = CGPoint(x: CGFloat(x), y: CGFloat(y))
 		original = forestScene.originalPoint(position)
 		println(abs(original.x - destination.x))
-		if original.x < destination.x{
-			println("Got to x dest")
+		var arrivedX = false
+		var arrivedY = false
+		if horizontalDirection == HorizontalDirection.Left{
+			if original.x <= destination.x{
+				println("Got to x dest")
+				arrivedX = true
+			}
+		}else{
+			if original.x >= destination.x{
+				println("Got to x dest")
+				arrivedX = true
+			}
 		}
-		if original.y < destination.y{
-			println("Got to y dest")
-			original.y = destination.y
+		if verticalDirection == VerticalDirection.Down{
+			if original.y <= destination.y{
+				println("Got to y dest")
+				arrivedY = true
+			}
+		}else{
+			if original.y >= destination.y{
+				println("Got to y dest")
+				arrivedY = true
+			}
 		}
-		if (original.x <= destination.x && original.y <= destination.y){
+		if arrivedX && arrivedY{
 			println("Got to destination")
 			removeAllActions()
 			texture = SKTexture(imageNamed: "bird-sitting_03")
 			location = BirdLocation.SittingOnLeft
 		}
 	}
-	func flyRight(amount: Float){
-		var x:Float = Float(position.x)
-		var y:Float = Float(position.y)
-		var original = forestScene.originalPoint(position)
-		var howMuch = forestScene.fact * ForestScene.backgroundWidth() / 10
-		if original.x < destination.x{
-			x += amount * 25
-			if x < -howMuch*2{
-				x += howMuch * 10
-			}
-			if x > howMuch*8{
-				x -= howMuch * 10
-			}
-		}
-		if original.y < destination.y{
-			y += amount * 25
-		}
-		
-		position = CGPoint(x: CGFloat(x), y: CGFloat(y))
-		original = forestScene.originalPoint(position)
-		println(abs(original.x - destination.x))
-		if original.x > destination.x{
-			println("Got to x dest")
-		}
-		if original.y > destination.y{
-			println("Got to y dest")
-			original.y = destination.y
-		}
-		if (original.x >= destination.x && original.y >= destination.y){
-			println("Got to destination")
-			removeAllActions()
-			texture = SKTexture(imageNamed: "bird-sitting_03")
-			location = BirdLocation.SittingOnRight
-		}
-	}
+	
+	
 	
 	func moveBy(amount: Float) {
 		switch location{
 		case .FlyingLeft:
-			flyLeft(amount)
+			fly(amount)
 			printGeometry()
 			break
 		case .FlyingRight:
-			flyRight(amount)
+			fly(amount)
 			break
 		default:
 			break
